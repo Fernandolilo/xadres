405b4e749e3728e50af814d97e7260398c529775
Fernandolilo <fernando.sistenpro@outlook.com>
2022-09-09 06:42:21 -0300

    First Class: Application and position

---
+++ src/xadres/application/Application.java
@@ -0,0 +1,14 @@
+package xadres.application;
+
+import xadres.application.bordgame.Position;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Position p = new Position(3,5);
+		System.out.println(p);
+
+	}
+
+}
