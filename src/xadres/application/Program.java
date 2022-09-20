31e9b1b17969b10b87dd1c5807289a55bab574c5
Fernandolilo <fernando.sistenpro@outlook.com>
2022-09-20 06:41:41 -0300

    Handing exceptions and clearing screen

--- src/xadres/application/Program.java
+++ src/xadres/application/Program.java
@@ -1,7 +1,9 @@
 package xadres.application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadres.application.bordgame.exceptions.ChessExceptions;
 import xadres.chess.ChessMathc;
 import xadres.chess.ChessPiece;
 import xadres.chess.ChessPosition;
@@ -10,23 +12,33 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		
-		//instanciando um tabuleiro		
-		ChessMathc cm = new ChessMathc();		
+		// instanciando um tabuleiro
+		ChessMathc cm = new ChessMathc();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			UI.printBoard(cm.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
+			try {
 
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
+				UI.clearScreen();
+				UI.printBoard(cm.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
 
-			ChessPiece capturedPiece = cm.performChessMove(source, target);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = cm.performChessMove(source, target);
+
+			} catch (ChessExceptions e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+
 		}
-		
 	}
 
 }
